a3e771adad710385a0763351925d124d9d2af7dc
dalgorithm <llsa0927@naver.com>
2022-11-15 22:51:01 +0900

    feat: 구매 참여 신청

--- src/main/java/com/dalgorithm/nbuy/product/repository/ProductRepository.java
+++ src/main/java/com/dalgorithm/nbuy/product/repository/ProductRepository.java
@@ -2,10 +2,19 @@ package com.dalgorithm.nbuy.product.repository;
 
 import com.dalgorithm.nbuy.product.entity.Product;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Lock;
+import org.springframework.data.jpa.repository.Query;
 
+import javax.persistence.LockModeType;
 import java.util.List;
 import java.util.Optional;
 
 public interface ProductRepository extends JpaRepository<Product, Long> {
     Optional<List<Product>> findByCategoryId(long categoryId);
+
+    List<Product> findByRecruiterId(String userId);
+
+    @Lock(value = LockModeType.PESSIMISTIC_WRITE)
+    @Query("select p from product p where p.id = :id")
+    Product findByIdWithPessimisticLock(Long id);
 }
